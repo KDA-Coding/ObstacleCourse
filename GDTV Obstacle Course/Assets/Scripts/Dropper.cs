1026f522267f9d46a98df42bcb3a5a2c8249b677
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2023-03-13 14:49:58 -0400

    Added Dropper Script
    Added Dropper script to control various obstacle objects that could be "dropped" into the obstacle course.

---
+++ GDTV Obstacle Course/Assets/Scripts/Dropper.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{
+
+    [SerializeField] float timeToWait = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > timeToWait)
+        {
+            Debug.Log("Three Seconds has elapsed.");
+        }
+    }
+}
